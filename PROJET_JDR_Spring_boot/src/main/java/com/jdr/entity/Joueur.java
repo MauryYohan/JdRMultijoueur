27848d81b5148d1ea49f4366ad1a988caef0f9b7
Yohan <ajc@DESKTOP-S6KD3FM>
2018-06-25 15:14:28 +0200

    f

--- PROJET_JDR_Spring_boot/src/main/java/com/jdr/entity/Joueur.java
+++ PROJET_JDR_Spring_boot/src/main/java/com/jdr/entity/Joueur.java
@@ -10,7 +10,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonView;
-//Just a change
+//Just a
 @Entity
 @Table(name="Joueur")
 @Inheritance(strategy = InheritanceType.JOINED)
