6cdf5d4babc39a92e54795f28456ea8f371f5117
ajc <ajc@DESKTOP-S6KD3FM>
2018-06-25 15:10:28 +0200

    commit

--- PROJET_JDR_Spring_boot/src/main/java/com/jdr/entity/Joueur.java
+++ PROJET_JDR_Spring_boot/src/main/java/com/jdr/entity/Joueur.java
@@ -10,7 +10,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonView;
-
+//Just a change
 @Entity
 @Table(name="Joueur")
 @Inheritance(strategy = InheritanceType.JOINED)
