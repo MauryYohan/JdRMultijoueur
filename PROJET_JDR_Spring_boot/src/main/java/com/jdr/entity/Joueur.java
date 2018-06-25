fdde19b04aae60d90272d81f8448de85eab5602c
User <User@DESKTOP-DC5LITC>
2018-06-25 15:53:34 +0200

    celine update

--- PROJET_JDR_Spring_boot/src/main/java/com/jdr/entity/Joueur.java
+++ PROJET_JDR_Spring_boot/src/main/java/com/jdr/entity/Joueur.java
@@ -11,6 +11,7 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonView;
 //Just a
+//Coucou
 @Entity
 @Table(name="Joueur")
 @Inheritance(strategy = InheritanceType.JOINED)
